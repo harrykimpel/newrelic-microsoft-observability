c6e65253690248e54b92c306d58c49822e28592b
Harry Kimpel <harrykimpel@hotmail.com>
2024-02-23 15:31:47 +0100

    Updating script to leverage MFA for the test user
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- user-login/README.md
+++ user-login/README.md
@@ -8,6 +8,14 @@ A typical request from customers often is: “I want to be able to understand ho
 - Latency of the connection to M365 websites.
 - A report that shows the latency and the availability over time (for example, the last 30 days).
 
+## Prerequisites
+
+In order for the Synthetic script to run, you need to create the following `Secure credentials` in New Relic Synthetic Monitoring section:
+
+- MSFT_USERNAME: the email address of the user to login to portal.office.com
+- MSFT_USER_PASSWORD: the password of the user to login to portal.office.com
+- MSFT_USERNAME_MFA_SECRET: the secret of the MFA authenticator app
+
 ## Installation
 
 The user login monitoring is implemented as a [New Relic Synthetics Scripted Browser](https://docs.newrelic.com/docs/synthetics/synthetic-monitoring/scripting-monitors/introduction-scripted-browser-monitors/) script.
@@ -17,3 +25,7 @@ The script that can be copied and pasted into the New Relic UI is available [her
 This repo also includes a sample New Relic dashboard that you can use to visualize the data in a meaningful way. The JSON representation of the dashboard is available [here](/user-login/user-login-dashboard.json).
 
 ![user login dashboard](/user-login/user-login-dashboard.png)
+
+## Setting up the MFA authenticator app
+
+When setting up MFA for the test user of this Synthetic script, you will be asked to scan the QR code for example from your mobile device. Choosing the `Can't scan the QR code` option during the setup process will provide you with a secret obtained while setting up 2FA/MFA for your user. Capture this key and store it in the Secure Credential `MSFT_USERNAME_MFA_SECRET` for use during monitor execution.
--- user-login/user-login.js
+++ user-login/user-login.js
@@ -5,7 +5,28 @@
  */
 
 var assert = require('assert');
-var DefaultTimeout = 30000;
+var DefaultTimeout = 60000;
+
+var otpauth = require('otpauth');
+
+// The secret is the value obtained while setting up 2FA/MFA for your user and choosing the Can't scan the QR code option.
+// Capture this key and store it in a Secure Credential for use during monitor execution.
+var secret = $secure.MSFT_USERNAME_MFA_SECRET;
+
+// This encoding and algorithm combination is used for Google Authenticator or Okta based TOTP.
+let totp = new otpauth.TOTP({
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: secret
+})
+
+// The scripted browser logic to navigate to the MFA screen would be located here. Find the object and then use this as the input to sendKeys.
+// Since this is time based, send the output to sendKeys directly instead of storing this in a variable at the top of your script.
+// Example: el.sendKeys(totp.generate()) where el is the element.
+// This quickstart logs out the token instead of sending it to an element or as input to an API test.
+//let token = totp.generate();
+//console.log(token);
 
 const logger = function ({ timeout: e = 18e4, stepLogging: t = !1, insightsKey: n = "" }) { const r = Date.now(); var s = 0, o = "", i = 0, a = 0; function l({ step: e = 0, msg: t = "", duration: r = 0, eventType: s = "SyntheticsCustom", custom: o = {} }) { if (void 0 === n || "" == n) return; var i = { method: "POST", headers: { "X-Insert-Key": n, "Content-Type": "application/json" }, data: { eventType: s, step: e, message: t, duration: r, JOB_ID: $env.JOB_ID, MONITOR_ID: $env.MONITOR_ID, ACCOUNT_ID: $env.ACCOUNT_ID, LOCATION: $env.LOCATION, PROXY_HOST: $env.PROXY_HOST, PROXY_PORT: $env.PROXY_PORT }, dataType: "text" }; const a = `https://insights-collector.newrelic.com/v1/accounts/${$env.ACCOUNT_ID}/events`; i.data = Object.assign({}, i.data, o), urlRequest(a, i) } function c(e, t, n = "") { e > a && 0 != a && _({ testCase: n }); let i = `Step ${e}: ${t} STARTED at ${s = Date.now() - r}ms. testCase=${n}`; console.log(i), o = t, a = e } function _({ testCase: i = "" }) { var c = Date.now() - r, _ = c - s; if (console.log(`Step ${a}: ${o} FINISHED. It took ${_}ms to complete. testCase=${i}`), t && n.length > 0 ? l({ step: a, msg: o, duration: _, custom: { testCase: i } }) : t && $util.insights.set(`Step ${a}: ${o}`, _), e > 0 && c > e) throw new Error("Script timed out. " + c + "ms is longer than script timeout threshold of " + e + "ms.") } return { logStep: function (e) { c(i++, e) }, log: c, getStep: function () { return i }, end: _, endTestCase: function (e = "") { var i = Date.now() - r - s; console.log(`Step ${a}: ${o} FINISHED. It took ${i}ms to complete.`), t && n.length > 0 ? l({ step: a, msg: o, duration: i, custom: { testCase: e } }) : t && $util.insights.set(`Step ${a}: ${o}`, i), $util.insights.set("testCase", e), $util.insights.set("testCaseStatus", "Pass"), t && n.length > 0 && l({ eventType: "SyntheticsTests", custom: { testCase: e, testCaseStatus: "Pass" } }) }, error: function (e, r = "") { console.log(`Error in Step ${a}: ${o}`), $util.insights.set("errorStep", "" + a), $util.insights.set("errorMsg", e.message), $util.insights.set("errorLineNumber", e.lineNumber), $util.insights.set("testCase", r), $util.insights.set("testCaseStatus", "fail"), t && n.length > 0 && l({ eventType: "SyntheticsTests", custom: { testCase: r, testCaseStatus: "Fail" } }) }, postInsights: l } }({})
 
@@ -19,14 +40,16 @@ $webDriver.getCapabilities().then(function () { })
             .then(function () { return logger.log(3, "Click By.id(\"idSIButton9\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("idSIButton9"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
             .then(function () { return logger.log(4, "Sleep a little", "M365"), $browser.sleep(1000) })
             .then(function () { return logger.log(5, "Type password", "M365"), $browser.waitForAndFindElement($selenium.By.id("i0118"), DefaultTimeout).then(e => (e.sendKeys($secure.MSFT_USER_PASSWORD), Promise.resolve(!0))) })
-            .then(function () { return logger.log(6, "Click By.id(\"idSIButton9\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("idSIButton9"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
-            .then(function () { return logger.log(7, "Click By.css(\"span\")", "M365"), $browser.waitForAndFindElement($selenium.By.css("span"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
-            .then(function () { return logger.log(8, "Click By.id(\"idBtn_Back\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("idBtn_Back"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
-            .then(function () { return logger.log(9, "Click By.id(\"officeHome__content\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("officeHome__content"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
+            .then(function () { return logger.log(6, "Click By.id(\"idSIButton9\")", "mfa"), $browser.waitForAndFindElement($selenium.By.id("idSIButton9"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
+            .then(function () { return logger.log(7, "Enter MFA token using locator By.id(\"idTxtBx_SAOTCC_OTC\")", "mfa"), $browser.waitForAndFindElement($selenium.By.id("idTxtBx_SAOTCC_OTC"), DefaultTimeout).then(e => (e.sendKeys(totp.generate()), Promise.resolve(!0))) })
+            .then(function () { return logger.log(8, "Click By.id(\"idSubmit_SAOTCC_Continue\")", "mfa"), $browser.waitForAndFindElement($selenium.By.id("idSubmit_SAOTCC_Continue"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
+            .then(function () { return logger.log(9, "Click By.id(\"idBtn_Back\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("idBtn_Back"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
+            .then(function () { return logger.log(10, "Click By.id(\"officeHome__content\")", "M365"), $browser.waitForAndFindElement($selenium.By.id("officeHome__content"), DefaultTimeout).then(e => (e.click(), Promise.resolve(!0))) })
             .then(function () {
                 logger.endTestCase("M365");
             }, function (err) {
                 logger.error(err, "M365");
                 throw (err);
             });
+
     })
